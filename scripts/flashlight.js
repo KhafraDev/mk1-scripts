40ef1afa9e46d077c8b3d1b8eb16482acc3b1ba3
Khafra <42794878+KhafraDev@users.noreply.github.com>
2020-03-22 13:29:42 -0400

    Update flashlight.js

--- scripts/flashlight.js
+++ scripts/flashlight.js
@@ -1,25 +1,31 @@
 /**
  * HWBUTTON-VOLUP
- * on volup, change the flashlight level if it's already on
- * on fully bright, set to 0 | otherwise, add .25 to its level
- * if no button presses for 10 seconds, go back to default functionality
+ * On press:
+ *  1. If 2 presses within 500ms are detected and the code was not running, turn on flashlight.
+ *  2. Otherwise, add 1 to the flashlight level, or if it's max, set to 0.
+ * After 10 seconds, revert to default functionality, until the 2 presses are detected.
  * 
- * will be updated once option to get+set volume is added
+ * Will be updated when get/set volume is added. 
  */
 
 var curr = flashlight.currentLevel();
-var timeout = setTimeout(() => {
-    isRunning = void 0; // the cooler undefined
-}, 10 * 1000);
+var VOLUP_PRESSES = typeof VOLUP_PRESSES === 'undefined' ? 0 : VOLUP_PRESSES;
 
-if(!isRunning && curr > 0) {
-    var isRunning = true; // run on first button press
-}
+// reset presses after 500ms
+setTimeout(() => VOLUP_PRESSES = 0, 500);
+
+var isRunning = curr > 0 || isRunning === true;
 
 if(isRunning) {
+    // set isRunning to undefined
+    setTimeout(() => isRunning = void 0, 10 * 1000);
+    
     if(curr === 1) {
         flashlight.setLevel(0);
     } else {
         flashlight.setLevel(curr + .25);
     }
+} else if(!isRunning && ++VOLUP_PRESSES >= 2) {
+    isRunning = true;
+    flashlight.setLevel(.25);
 }
